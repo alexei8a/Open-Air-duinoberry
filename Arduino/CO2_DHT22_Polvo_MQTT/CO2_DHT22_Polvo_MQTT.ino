66ef8a4ec3cb242c456a1496296ced85fba632a7
Alexei Ochoa Duarte <agochoad@unal.edu.co>
2017-09-25 19:33:23 -0500

    Update CO2_DHT22_Polvo_MQTT.ino

--- Arduino/CO2_DHT22_Polvo_MQTT/CO2_DHT22_Polvo_MQTT.ino
+++ Arduino/CO2_DHT22_Polvo_MQTT/CO2_DHT22_Polvo_MQTT.ino
@@ -8,20 +8,13 @@
 #define TOPIC2 "arduinoUNOWifi/DHT22"
 #define TOPIC3 "arduinoUNOWifi/GP2Y1010AU0F"
 
-int pinDHT22 = 4;
+int pinDHT22 = 4; //Conectar el sensor DHT22 al pin 4 
 SimpleDHT22 dht22;
 
-int measurePin = 1; //Connect dust sensor to Arduino A1 pin
-int ledPower = 3;   //Connect 3 led driver pins of dust sensor to Arduino D2
+int measurePin = 1; //Conectar la salida del sensor de polvo al pin A1 de la Arduino 
+int ledPower = 3;   //Conecte el pin que controla el LED del sensor de polvo a la entrada D3 de la Arduino
  
-int samplingTime = 280;
-int deltaTime = 40;
-int sleepTime = 9680;
- 
-float voMeasured = 0;
-float calcVoltage = 0;
-float dustDensity = 0;
- 
+
 
 void setup() {
   Ciao.begin();
@@ -30,10 +23,15 @@ void setup() {
 }
 
 void loop() {
-  
-  int mq135_adc = analogRead(A0);
-  float mq135_voltaje = mq135_adc * (5.0 / 1023.0);
-  float mq135_resistencia = 1000*((5-mq135_voltaje)/mq135_voltaje);
+
+//Se hacen 5 mediciones para evitar valores extremos causados por perturbaciones 
+  float sum2=0;
+  for (int i=0;i<=4;i++){
+  sum2=sum2+getVoltaje();  
+  }
+ 
+  float mq135_voltaje=sum2/5;
+  float mq135_resistencia = 2000*((5-mq135_voltaje)/mq135_voltaje);
   double dioxidoDeCarbono = 245*pow(mq135_resistencia/5463, -2.26);
   double oxidosDeNitrogeno = 132.6*pow(mq135_resistencia/5463, -2.74);
   double amoniaco = 161.7*pow(mq135_resistencia/5463, -2.26);
@@ -76,28 +74,13 @@ void loop() {
   Ciao.write(CONNECTOR, TOPIC2, payload2);
 
 
-
-  digitalWrite(ledPower,LOW); // power on the LED
-  delayMicroseconds(samplingTime);
-  voMeasured = analogRead(measurePin); // read the dust value 
-  delayMicroseconds(deltaTime);
-  digitalWrite(ledPower,HIGH); // turn the LED off
-  delayMicroseconds(sleepTime);
- 
-  // 0 - 5V mapped to 0 - 1023 integer values
-  // recover voltage
-  calcVoltage = voMeasured * (5.0 / 1024.0);
- 
-  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
-  // Chris Nafis (c) 2012
-  dustDensity = 0.17 * calcVoltage - 0.1;
- 
-  Serial.print("Raw Signal Value (0-1023): ");
-  Serial.print(voMeasured);
- 
-  Serial.print(" - Voltage: ");
-  Serial.print(calcVoltage);
- Serial.println();
+ //Se hacen 5 mediciones para evitar valores extremos causados por perturbaciones 
+  float sum=0;
+  for (int i=0;i<=4;i++){
+  sum=sum+getDustDensity();  
+  }
+  
+  float dustDensity=sum/5;
   String payload3 = "{";
   payload3 += "\"densidad de polvo\":";
   payload3 += dustDensity; 
@@ -109,6 +92,44 @@ void loop() {
 
  
   delay(30000);
+}
+
+//Función que retorna el voltaje medido por el MQ135
+float getVoltaje(){
+  int mq135_adc = analogRead(A0);
+  float mq135_voltaje = mq135_adc * (5.0 / 1023.0);
+  return mq135_voltaje;
+}
+
+//Función que retorna la densidad de polvo en el aire
+float getDustDensity(){  
+  int samplingTime = 280;
+  int deltaTime = 40;
+  int sleepTime = 9680;
+  float voMeasured = 0;
+  float calcVoltage = 0;
+  float dustDensity = 0;
+  digitalWrite(ledPower,LOW); // encender el LED
+  delayMicroseconds(samplingTime);
+  voMeasured = analogRead(measurePin); // leer el valor análogo del sensor 
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH); // apagar el LED 
+  delayMicroseconds(sleepTime);
+ 
+  // Convierte las medidas análogas de 0 - 5V en valores enteros de 0 - 1023 
+  // recupera el voltaje
+  calcVoltage = voMeasured * (5.0 / 1024.0);
+ 
+  // Ecuación lineal tomada de http://www.howmuchsnow.com/arduino/airquality/
+  dustDensity = 0.17 * calcVoltage - 0.1;
+ 
+  Serial.print("Raw Signal Value (0-1023): ");
+  Serial.print(voMeasured);
+ 
+  Serial.print(" - Voltage: ");
+  Serial.print(calcVoltage);
+  Serial.println();
+  return dustDensity;
 }
 
 
